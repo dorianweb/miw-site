5a525be643febaf124cc08e72551da06b40e0c3a
dorianweb <37084529+dorianweb@users.noreply.github.com>
2020-04-10 02:22:37 +0200

    menu bien avancer

--- resources/js/components/NavbarComponent.vue
+++ resources/js/components/NavbarComponent.vue
@@ -1,33 +1,37 @@
 <template>
 
-
-    <nav class="menu">
-        <div class="menu_head">
-            <div class="menu_head_logo">
-                <svg>
-                    <use xlink:href="#logo"></use>
-                </svg>
+    <div>
+        <nav class="menu">
+            <div class="menu_head">
+                <div class="menu_head_logo">
+                    <svg>
+                        <use xlink:href="#logo"></use>
+                    </svg>
+                </div>
+                <label class="menu_head_icon" for="menutoggle">
+                    <svg v-show="!isMobileMenuOpen">
+                        <use xlink:href="#menu"></use>
+                    </svg>
+                    <svg v-show="isMobileMenuOpen">
+                        <use xlink:href="#cross"></use>
+                    </svg>
+                </label>
+                <input type="checkbox" id="menutoggle" v-model="isMobileMenuOpen" style="display:none">
             </div>
-            <label class="menu_head_icon" for="menutoggle">
-                <svg v-show="!isMobileMenuOpen">
-                    <use xlink:href="#menu"></use>
-                </svg>
-                <svg v-show="isMobileMenuOpen">
-                    <use xlink:href="#cross"></use>
-                </svg>
-            </label>
-            <input type="checkbox" id="menutoggle" v-model="isMobileMenuOpen" style="display:none">
-        </div>
+            <div v-show="isMobileMenuOpen" class="menu_body">
+                <ul class="menu_content">
+                    <li v-for="route in  routes" class="link">
+                        <div class="link_bg"></div>
+                        <router-link :to="route.path" class="link_item">
+                            {{route.name}}
+                        </router-link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div class="test"></div>
+    </div>
 
-        <div v-show="isMobileMenuOpen" class="menu_body">
-            <ul class="">
-                <li class="" v-for="route in  routes">
-                    <router-link :to="route.path" class="">{{route.name}}</router-link>
-                </li>
-            </ul>
-        </div>
-
-    </nav>
 </template>
 
 <script>
@@ -48,16 +52,19 @@
         width: 100%;
         height: 100%;
     }
+
     ul {
         padding: 0;
     }
-    li{
+
+    li {
         list-style-type: none;
     }
 
     .menu {
         width: 100%;
         height: 100%;
+        position: fixed;
     }
 
     .menu_head {
@@ -69,7 +76,6 @@
         justify-content: space-between;
         border-bottom: black solid 1px;
     }
-
 
     .menu_head_logo {
         width: 90px;
@@ -86,8 +92,42 @@
         margin-bottom: 0;
         margin-right: 10px;
     }
-    .menu_head_icon>svg{
+
+    .menu_head_icon > svg {
         fill: white;
     }
 
+    .link {
+        display: flex;
+        width: 100%;
+        height: 50px;
+        position: relative;
+        margin-bottom: 8px;
+    }
+
+    .link_bg {
+        width: 100%;
+        height: 40px;
+        position: absolute;
+        background-color: #CFE9FF;
+    }
+
+    .link_item {
+        width: 100%;
+        height: 40px;
+        position: absolute;
+        background-color: #2699FB;
+        top: 6px;
+        left: 8px;
+        text-decoration: none;
+        text-align: center;
+        font-size: 1.5em;
+        color: white;
+    }
+
+    .test {
+        width: 100%;
+        height: 900px;
+        background-color:black;
+    }
 </style>
