21dab49dcb0ec943544336044d3fc28c8d8cc52e
dorianweb <37084529+dorianweb@users.noreply.github.com>
2020-06-18 23:39:17 +0200

    Update bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -21,7 +21,8 @@ try {
 window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios.defaults.withCredentials = true;
-window.axios.defaults.baseURL = 'http://miw/';
+window.axios.defaults.baseURL = 'https://miw-site.herokuapp.com/'
+
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
